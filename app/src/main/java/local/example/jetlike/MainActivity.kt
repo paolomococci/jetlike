ee9c71caf6966d67f599b01421e32e629548fdef
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-03-21 22:15:25 +0100

    Update MainActivity.kt

--- app/src/main/java/local/example/jetlike/MainActivity.kt
+++ app/src/main/java/local/example/jetlike/MainActivity.kt
@@ -20,11 +20,12 @@ package local.example.jetlike
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import local.example.jetlike.R.layout.activity_main
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activity_main)
     }
 }
