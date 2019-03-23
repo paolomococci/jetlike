522d349d57f2b29c4ee2e74ed4f4fabfe65da496
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-03-23 07:14:38 +0100

    Update StartViewModel.kt

--- app/src/main/java/local/example/jetlike/StartViewModel.kt
+++ app/src/main/java/local/example/jetlike/StartViewModel.kt
@@ -18,8 +18,14 @@
 
 package local.example.jetlike
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel;
 
 class StartViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _data = MutableLiveData<String>()
+    val data: LiveData<String> get() = _data
+    init {
+        _data.value = "Hello world!"
+    }
 }
