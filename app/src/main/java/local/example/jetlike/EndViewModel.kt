6bdd92b0cb755439f7e9b8ddc1fba6acd3bbd09f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-03-23 07:16:00 +0100

    Update EndViewModel.kt

--- app/src/main/java/local/example/jetlike/EndViewModel.kt
+++ app/src/main/java/local/example/jetlike/EndViewModel.kt
@@ -18,8 +18,14 @@
 
 package local.example.jetlike
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel;
 
 class EndViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _data = MutableLiveData<String>()
+    val data: LiveData<String> get() = _data
+    init {
+        _data.value = "Bye!"
+    }
 }
